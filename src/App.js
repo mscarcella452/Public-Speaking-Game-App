7dbd25f37c0e9b146d05ef165ac57e0235aa2cfb
Matt Scarcella <mscarcella452@gmail.com>
2023-03-20 18:25:18 -0400

    Added fonts, style folder with SXstyles files and gamePage file

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Paper } from "@mui/material";
+import { Sx, flexBoxSx } from "./Styles/SXstyles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import GamePage from "./Pages/GamePage";
+
+export const theme = createTheme({
+  breakpoints: {
+    values: {
+      galaxyFold: 0,
+      mobile: 300,
+      xs: 389,
+      sm: 625,
+      md: 715,
+      lg: 875,
+      xl: 1100,
+    },
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper
+        sx={{
+          ...flexBoxSx,
+          height: "100vh",
+          boxSizing: "border-box",
+          fontFamily: Sx.font.display,
+          background: Sx.color.primary,
+          overflow: "scroll",
+        }}
+      >
+        <Routes>
+          <Route path='*' element={<GamePage />} />
+        </Routes>
+      </Paper>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/Styles/SXstyles.js
@@ -0,0 +1,24 @@
+export const flexBoxSx = {
+  height: "100%",
+  width: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  boxSizing: "border-box",
+};
+
+export const Sx = {
+  font: {
+    display: "'Sunny Spells Basic', sans-serif",
+    secondary: "'Averia Libre', cursive",
+    number: "'Tilt Neon', cursive",
+    card: "'Neucha', cursive",
+  },
+  color: {
+    // fail: "#981010",
+    primary: "#457b9d",
+    secondary: "#be2f3a",
+    success: "#0ead69",
+    fail: "#9d6745",
+  },
+};
